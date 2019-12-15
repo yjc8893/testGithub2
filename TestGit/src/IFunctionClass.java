e5b42564d1217d90d8b832c999390f9a133e9ebe
yjc8893 <yjc8893@gmail.com>
2019-12-15 23:34:21 +0800

    first commit

---
+++ TestGit/src/IFunctionClass.java
@@ -0,0 +1,5 @@
+
+public interface IFunctionClass {
+
+	public int sum(int num1, int num2);
+}
---
+++ TestGit/src/StartUp.java
@@ -0,0 +1,10 @@
+
+public class StartUp {
+
+	public static void main(String[] args) {
+		IFunctionClass f1 = new FunctionClass();
+		System.out.println(f1.sum(1, 2));
+
+	}
+
+}
